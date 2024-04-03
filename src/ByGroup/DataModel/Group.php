7c375ffcb776deb8224c9916542db3059537494a
AP <anton@leaptheory.com>
2024-04-03 14:57:52 -0700

    add active period object to the Group object

--- src/ByGroup/DataModel/Group.php
+++ src/ByGroup/DataModel/Group.php
@@ -8,6 +8,7 @@ class Group
         public readonly int  $group,
         public readonly int  $periods_count,
         public readonly bool $active_now,
+        public readonly ?Period $active_period = null,
     )
     {
     }
